aa90fbb8321c5dd22a96270285f95b01d17d344f
D-ongyoon <147066945+D-ongyoon@users.noreply.github.com>
2023-10-11 08:24:03 +0900

    [level unrated] Title: 짝수의 합, Time: 1.11 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/unrated/120831. 짝수의 합/README.md
+++ 프로그래머스/unrated/120831. 짝수의 합/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 짝수의 합 - 120831 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120831) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120831?language=java) 
 
 ### 성능 요약
 
-메모리: 77.9 MB, 시간: 0.02 ms
+메모리: 74.9 MB, 시간: 1.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 3일 8:15:20
+2023년 10월 3일 8:24:2
 
 ### 문제 설명
 
--- 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.java
@@ -1,10 +1,11 @@
+import java.util.stream.IntStream;
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for (int i=1; i<=n; i++) {
-            if (i%2 == 0) {
-                answer += i;
-            }
+        if (n > 0 && n <= 1000) {
+         answer = IntStream.rangeClosed(0, n)
+            .filter(i -> i % 2 == 0)
+            .sum();
         }
         return answer;
     }
