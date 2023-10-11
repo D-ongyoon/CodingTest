227d978f611f49e0ec07eabeacf09459e2ff78b8
D-ongyoon <147066945+D-ongyoon@users.noreply.github.com>
2023-10-11 09:05:33 +0900

    [level unrated] Title: 배열의 평균값, Time: 1.78 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.java
@@ -2,8 +2,7 @@ import java.util.stream.Stream;
 import java.util.Arrays;
 class Solution {
     public double solution(int[] numbers) {
-        double answer = 0;
-        answer = Arrays.stream(numbers).average().orElse(0);
+        double answer = Arrays.stream(numbers).average().orElse(0);
         return answer;
     }
 }
