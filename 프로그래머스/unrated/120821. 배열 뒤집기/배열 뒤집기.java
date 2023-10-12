22715b6dbd1562aa1106cd15673adec5307a45ea
D-ongyoon <147066945+D-ongyoon@users.noreply.github.com>
2023-10-12 13:42:02 +0900

    [level unrated] Title: 배열 뒤집기, Time: 3.88 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,9 +1,16 @@
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[num_list.length];
-        for(int i=0; i<num_list.length; i++) {
-            answer[i] = num_list[num_list.length-i-1];
-        }
+        int[] answer = {};
+        
+        List<Integer> list = Arrays.stream(num_list).boxed().collect(Collectors.toList());
+        Collections.reverse(list);
+        answer = list.stream().mapToInt(Integer::intValue).toArray();
+        
         return answer;
     }
 }
