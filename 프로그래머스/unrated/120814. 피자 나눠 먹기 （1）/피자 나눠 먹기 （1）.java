b5e8ab0a9fb514e797cd230ae96718f95ce867f0
D-ongyoon <147066945+D-ongyoon@users.noreply.github.com>
2023-10-16 08:02:28 +0900

    [level unrated] Title: 피자 나눠 먹기 (1), Time: 0.19 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/unrated/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,11 +1,7 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-         if (n%7 == 0) {
-             answer = n/7;
-         } else {   
-            answer = (n/7) + 1;
-        }
+        answer = (int) Math.ceil((double) n/7);
         return answer;
     }
 }
