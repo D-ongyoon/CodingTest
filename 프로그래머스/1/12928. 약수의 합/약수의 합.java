9c5ea7945787d58c7b0eacbc6bbcc102aa823a94
D-ongyoon <147066945+D-ongyoon@users.noreply.github.com>
2023-10-17 08:16:53 +0900

    [level 1] Title: 약수의 합, Time: 1.32 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,9 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        answer = IntStream.rangeClosed(1,n).filter(i -> n % i == 0).sum();
+        return answer;
+    }
+}
