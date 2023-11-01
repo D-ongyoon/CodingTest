4a81a72a2df637a6a374834aceae429be339417d
D-ongyoon <147066945+D-ongyoon@users.noreply.github.com>
2023-11-02 07:47:25 +0900

    [level 1] Title: 음양 더하기, Time: 11.74 ms, Memory: 88.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 123456789;
+        for (int i = 0; i < absolutes.length; i++) {
+            if (signs[i] == false) {
+                absolutes[i] = absolutes[i] * -1;
+            }
+            answer = Arrays.stream(absolutes).sum();
+        }
+        return answer;
+    }
+}
