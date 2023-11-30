dbb7987a680948702e2d2cea25a104bb1f7ac50a
D-ongyoon <147066945+D-ongyoon@users.noreply.github.com>
2023-12-01 07:17:41 +0900

    [level unrated] Title: 원소들의 곱과 합, Time: 1.51 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
-        answer=  (Arrays.stream(num_list).reduce((acc, i) -> acc * i).getAsInt() < Math.pow(Arrays.stream(num_list).sum(), 2)) ? 1 : 0;
+        answer = (Arrays.stream(num_list).reduce((x, y) -> x * y).getAsInt() < Math.pow(Arrays.stream(num_list).sum(), 2)) ? 1 : 0;
         return answer;
     }
 }
