b869badb896af2cc9e2e0c095678dbcda345105b
D-ongyoon <147066945+D-ongyoon@users.noreply.github.com>
2023-12-04 08:49:05 +0900

    [level unrated] Title: 짝수 홀수 개수, Time: 2.98 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -1,10 +1,13 @@
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[2];
-        answer[0] = (int) Arrays.stream(num_list).filter(x -> x % 2 == 0).count();
-        answer[1] = (int) Arrays.stream(num_list).filter(x -> x % 2 != 0).count();
+        int[] answer = {};
+        answer = IntStream.of(
+                (int) Arrays.stream(num_list).filter(x -> x % 2 == 0).count()
+              , (int) Arrays.stream(num_list).filter(x -> x % 2 == 1).count()
+        ).toArray();
         return answer;
     }
 }
