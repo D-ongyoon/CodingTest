bc084df9af05e5e533b48e17bf85e8a6a6bc09c8
D-ongyoon <147066945+D-ongyoon@users.noreply.github.com>
2023-12-20 07:33:29 +0900

    [level unrated] Title: 첫 번째로 나오는 음수, Time: 1.66 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/README.md
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 첫 번째로 나오는 음수 - 181896 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181896?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181896) 
 
 ### 성능 요약
 
-메모리: 74.3 MB, 시간: 0.01 ms
+메모리: 71.8 MB, 시간: 1.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 3일 7:31:5
+2023년 12월 3일 7:33:27
 
 ### 문제 설명
 
--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -1,10 +1,9 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int[] num_list) {
-         for (int i = 0; i < num_list.length; i++) {
-            if (num_list[i] < 0) {
-                return i;
-            }
-        }
-        return -1;
+        int answer = 0;
+        answer = IntStream.range(0, num_list.length).filter(i -> num_list[i] < 0).findFirst().orElse(-1);
+        return answer;
     }
 }
