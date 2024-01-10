aff38279a28cd265dff91e1cadb9e6094fed091e
D-ongyoon <147066945+D-ongyoon@users.noreply.github.com>
2024-01-11 08:16:03 +0900

    [level unrated] Title: 배열 자르기, Time: 1.78 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = {};
+        answer = Arrays.stream(numbers).skip(num1).limit(num2 - num1 + 1).toArray();
+        return answer;
+    }
+}
