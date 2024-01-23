558ee41f2ac2cb16715976ef95edb2f240ed45f7
D-ongyoon <147066945+D-ongyoon@users.noreply.github.com>
2024-01-24 08:20:10 +0900

    [level 0] Title: 세균 증식, Time: 0.78 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -1,10 +1,9 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int n, int t) {
         int answer = 0;
-        for (int i=0; i<t; i++) {
-            n *= 2;
-        }
-        answer = n;
+        answer = IntStream.range(0, t).reduce(n, (a, b) -> a * 2);
         return answer;
     }
 }
